5b61b53f7dd5be270197266acfabf8144753cc4d
Intropyro1 <Alan.Bleboo1@marist.edu>
2025-05-20 15:24:01 -0400

    modified the package.json,package-lock.json,tsconfig.json and app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import SearchBar from "./SearchBarComponents/searchBar";
 import SearchResults from "./SearchresultsComponent/SearchResults";
 import PlaylistComponents from "./PlayListComponents/PlaylistComponents";
